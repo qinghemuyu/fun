94d6a57455f5c28181de71633a27c60a2d86d264
清河 <z17694468095@gmail.com>
2022-08-23 08:45:52 +0000

    云载本体
    Signed-off-by: 清河 <z17694468095@gmail.com>

---
+++ 清河公开云载2.0.lua
@@ -0,0 +1,215 @@
+local function mainrunQInEmax()
+    client.Command("clear", true)
+    client.Command("toggleconsole", true)
+    client.Command("echo \" QInEmax公开版云载已加载---" .. client.GetConVar('name') .. "--祝你游玩愉快~+-+\"", true)
+    client.Command("echo \" 当前版本2.0   \"", true)
+    client.Command("echo \" Code and CFG by 清河 QQ1348984838 \"", true)
+    client.Command("echo \" ◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◆◆◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◆◆◆◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◆◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◇◆◆◇◇◇◇◇◇◇◇◇◆◆◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◆◆◆◇◇◇◇◇◇◇◇◇◇◇◇◆◆◆◆◆◆◆◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◇◇◆◆◆◇◇◇◇◇◇◇◇◆◇◇◆◆◆◆◇◇◇◇◇◇◇◇◇◇◇◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◇◇◇◆◆◆◇◆◆◆◆◆◆◆◆◆◆◆◆◆◆◇◇◇◇◇◇◇◇◇◇◇◆◆◆◆◆◆◆◆◆◆◆◆◆◇◆◆◆◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◇◇◇◆◆◆◇◆◆◆◆◆◆◆◆◆◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◆◆◇◆◇◇◇◇◇◇◇◇◇◇◆◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◇◇◇◇◆◆◇◇◇◇◇◇◇◆◆◆◆◆◆◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◆◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◇◇◇◇◇◇◇◇◆◆◆◆◆◆◆◆◆◆◆◆◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◆◆◇◇◇◇◆◆◇◇◆◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◆◇◇◇◇◇◆◆◇◇◆◆◆◆◆◇◇◇◇◇◇◆◆◇◇◇◇◇◇◇◆◆◇◇◇◇◇◇◆◆◆◆◆◆◆◆◇◇◆◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◆◆◆◇◇◇◆◆◇◇◇◆◆◆◆◆◆◆◆◆◆◆◆◆◇◇◇◇◇◇◆◆◆◆◇◇◆◆◇◆◆◆◆◆◆◆◇◇◆◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◆◆◆◇◇◆◆◆◆◆◆◆◆◆◆◆◆◆◆◇◇◇◇◇◇◇◇◇◇◇◆◆◆◆◇◆◆◇◆◇◇◇◆◆◆◇◇◆◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◇◆◆◇◆◆◆◆◆◆◇◇◇◇◇◇◇◆◆◇◇◇◇◇◇◇◇◇◇◇◇◇◆◆◇◆◇◇◆◇◇◇◆◆◇◇◇◆◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◇◆◆◇◆◆◇◇◆◆◆◆◆◆◆◆◆◆◆◇◇◇◇◇◇◇◇◇◇◇◇◇◆◇◆◆◇◇◆◇◇◇◆◆◇◇◇◆◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◇◇◇◇◆◆◇◇◇◆◆◆◆◆◆◆◆◆◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◆◆◇◇◆◆◆◆◆◆◆◇◇◆◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◇◇◇◆◆◇◇◇◇◆◇◇◇◇◇◆◇◆◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◆◆◇◇◆◆◆◆◆◆◆◆◇◇◆◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◇◇◇◆◆◇◇◇◇◆◆◆◆◆◆◆◆◆◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◆◆◇◇◆◆◇◇◇◇◇◇◇◇◆◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◇◇◇◆◆◇◇◇◇◆◆◆◆◆◆◆◇◆◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◆◆◇◇◇◆◇◇◇◇◇◇◇◇◆◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◇◇◆◆◇◇◇◇◇◆◇◇◇◆◆◆◇◆◇◇◇◇◇◇◇◇◇◇◇◇◇◇◆◆◇◇◇◇◇◇◇◇◇◇◇◇◇◆◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◆◇◇◆◆◇◇◇◇◇◆◆◆◆◆◆◆◆◆◇◇◇◇◇◇◇◇◇◇◇◆◇◇◆◆◇◇◇◇◇◇◇◇◇◇◇◇◇◆◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◆◆◆◆◆◇◇◇◇◇◆◆◆◇◇◇◇◇◆◇◇◇◇◇◇◇◇◇◇◇◆◆◆◆◆◇◇◇◇◇◇◇◇◇◇◇◇◇◆◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◆◆◆◆◇◇◇◇◇◆◇◇◇◇◇◇◇◆◇◇◇◇◇◇◇◇◇◇◇◇◆◆◆◇◇◇◇◇◇◇◇◇◆◆◇◇◇◆◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◇◆◆◇◇◇◇◇◇◆◇◇◇◆◆◆◆◆◇◇◇◇◇◇◇◇◇◇◇◇◆◆◆◇◇◇◇◇◇◇◇◇◆◆◆◆◆◆◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◇◆◆◇◇◇◇◇◆◆◇◇◇◇◆◆◆◆◇◇◇◇◇◇◇◇◇◇◇◇◇◆◆◇◇◇◇◇◇◇◇◇◇◇◆◆◆◆◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◇◇◇◇◇◇◇◇◆◆◇◇◇◇◇◆◆◆◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◆◆◆◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◇◇◇◇◇◇◇◇◇◆◇◇◇◇◇◇◆◆◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◆◆◇◇◇◇◇◇◇◇ \"", true)
+    client.Command("echo \" ◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇◇ \"", true)
+end
+mainrunQInEmax()
+local loadnumload=http.Get( "http://1.117.227.75/add_load" )
+    local QInEMaxCFGInvisible,QInEMaxLuaInvisible,QInEMaxMiscInvisible=false,true,true
+local p='"'
+local result={};
+local legitname={};
+local legiturl={}
+local ragename={}
+local rageurl={}
+local hvhname={}
+local hvhurl={}
+local aw_menu = gui.Reference("MENU");
+local Windowicon=http.Get( "http://1256182033.vod2.myqcloud.com/7d2fb881vodcq1256182033/df0e3ca65285890812571336035/em0xc8ShsnQA.png" )
+local RGBA, width, height = common.DecodePNG(Windowicon);
+local texture = draw.CreateTexture(RGBA, width, height)
+function string.split(inputstr, sep)
+    if sep == nil then sep = "%s" end
+    local t = {}
+    for str in string.gmatch(inputstr, "([^"..sep.."]+)") do table.insert(t, str) end
+    return t
+end
+Window=gui.Window( "Window", "fix清河公开云载2.0", 600, 600, 315, 0 )
+Window:SetIcon(texture)
+local QInEMaxCFG=gui.Groupbox( Window, "CFG", 95, 10, 215, 0 )
+local QInEMaxLua=gui.Groupbox( Window, "Luas", 95, 10, 215, 0 )
+local QInEMaxMisc=gui.Groupbox( Window, "Misc", 95, 10, 215, 0 )
+local WindowTab=gui.Groupbox( Window, "QInEMax",10, 10 , 80 , 0)
+local WindowTabBtt_1=gui.Button( WindowTab, "CFG", function ()
+    QInEMaxCFGInvisible=false
+    QInEMaxLuaInvisible=true
+    QInEMaxMiscInvisible=true
+end)
+local WindowTabBtt_2=gui.Button( WindowTab, "Luas", function ()
+    QInEMaxCFGInvisible=true
+    QInEMaxLuaInvisible=false
+    QInEMaxMiscInvisible=true
+end )
+local WindowTabBtt_3=gui.Button( WindowTab, "Misc", function ()
+    QInEMaxCFGInvisible=true
+    QInEMaxLuaInvisible=true
+    QInEMaxMiscInvisible=false
+end )
+local text=gui.Text( QInEMaxMisc, http.Get( "https://gitee.com/muyuyanchen/public-cloud2.0/raw/master/aaaaaa.txt?download=false" ) )
+local Buttonaa=gui.Button( QInEMaxMisc, "QInEmax", function ()
+    panorama.RunScript( [[
+        SteamOverlayAPI.OpenExternalBrowserURL("http://1.117.227.75/index.jsp");
+    ]] )
+end )
+local legitlist=gui.Listbox( QInEMaxCFG, "legitlist", 80, ... )
+local ragelist=gui.Listbox( QInEMaxLua, "ragelist", 80, ... )
+local legitlistf5=gui.Button( QInEMaxCFG, "刷新", function ()
+    result={}--重置
+    result = string.split(http.Get( "https://gitee.com/muyuyanchen/public-cloud2.0/raw/master/cfglist.txt?download=false"), p)
+    for i = 1, #result do
+        legitname[i]=result[i]
+    end
+    legitlist:SetOptions( unpack(legitname) )
+    result={}--重置
+    result = string.split(http.Get( "https://gitee.com/muyuyanchen/public-cloud2.0/raw/master/cfgurl.txt?download=false"), p)
+    for i = 1, #result do
+        legiturl[i]=result[i]
+    end
+end )
+local ragelistf5=gui.Button( QInEMaxLua, "刷新", function ()
+    result={}--重置
+    result = string.split(http.Get( "https://gitee.com/muyuyanchen/public-cloud2.0/raw/master/lualist.txt?download=false"), p)
+    for i = 1, #result do
+        ragename[i]=result[i]
+    end
+    ragelist:SetOptions( unpack(ragename) )
+    result={}--重置
+    result = string.split(http.Get( "https://gitee.com/muyuyanchen/public-cloud2.0/raw/master/luaurllist.txt?download=false"), p)
+    for i = 1, #result do
+        rageurl[i]=result[i]
+    end
+end )
+
+local function mianrun()
+    result={}--重置
+    result = string.split(http.Get( "https://gitee.com/muyuyanchen/public-cloud2.0/raw/master/cfglist.txt?download=false"), p)
+    for i = 1, #result do
+        legitname[i]=result[i]
+    end
+    legitlist:SetOptions( unpack(legitname) )
+    result={}--重置
+    result = string.split(http.Get( "https://gitee.com/muyuyanchen/public-cloud2.0/raw/master/lualist.txt?download=false"), p)
+    for i = 1, #result do
+        ragename[i]=result[i]
+    end
+    ragelist:SetOptions( unpack(ragename) )
+    result={}--重置
+    result = string.split(http.Get( "https://gitee.com/muyuyanchen/public-cloud2.0/raw/master/cfgurl.txt?download=false"), p)
+    for i = 1, #result do
+        legiturl[i]=result[i]
+    end
+    result={}--重置
+    result = string.split(http.Get( "https://gitee.com/muyuyanchen/public-cloud2.0/raw/master/luaurllist.txt?download=false"), p)
+    for i = 1, #result do
+        rageurl[i]=result[i]
+    end
+end
+mianrun()
+local function luamianrun()
+    for i = 1, #rageurl do
+        local DL = file.Open("dhasjkhdjkahsjkdh".. i ..".lua", "w");
+        DL:Write(http.Get("".. rageurl[i]..""));
+        DL:Close();
+        LoadScript( "dhasjkhdjkahsjkdh".. i ..".lua" )
+        local p=file.Delete( "dhasjkhdjkahsjkdh".. i ..".lua" )
+        ragename[i]="[已加载]"..ragename[i]
+        
+    end
+    ragelist:SetOptions( unpack(ragename) )
+end
+
+local legitload=gui.Button( QInEMaxCFG, "加载", function ()
+    local DL = file.Open("dhasjkhdjkahsjkdh.cfg", "w");
+    DL:Write(http.Get("".. legiturl[legitlist:GetValue()+1]..""));
+    DL:Close();
+    gui.Command( "cfg.load dhasjkhdjkahsjkdh.cfg" )
+    local p=file.Delete( "dhasjkhdjkahsjkdh.cfg" )
+    if legitlist:GetValue()==1 then
+        luamianrun()
+    end
+    if legitlist:GetValue()==4 then
+        print(legiturl[legitlist:GetValue()+1])
+        local DL = file.Open("一键加载登录.lua", "w");
+        DL:Write(http.Get("".. legiturl[legitlist:GetValue()+1]..""));
+        DL:Close();
+    LoadScript( "一键加载登录.lua" )
+    local p=file.Delete( "一键加载登录.lua" )
+    end
+    
+end )
+
+local rageload=gui.Button( QInEMaxLua, "加载", function ()
+    local DL = file.Open("dhasjkhdjkahsjkdh".. tostring(ragelist:GetValue()+1) ..".lua", "w");
+    DL:Write(http.Get("".. rageurl[ragelist:GetValue()+1]..""));
+    DL:Close();
+    LoadScript( "dhasjkhdjkahsjkdh".. tostring(ragelist:GetValue()+1) ..".lua" )
+    local p=file.Delete( "dhasjkhdjkahsjkdh".. tostring(ragelist:GetValue()+1) ..".lua" )
+    ragename[ragelist:GetValue()+1]="[已加载]"..ragename[ragelist:GetValue()+1]
+    ragelist:SetOptions( unpack(ragename) )
+end )
+local unloadall=gui.Button( QInEMaxLua, "卸载", function ()
+    UnloadScript( "dhasjkhdjkahsjkdh".. tostring(ragelist:GetValue()+1) ..".lua" )
+    
+    if string.split(ragename[ragelist:GetValue()+1],"]")[2]==nil then
+        ragename[ragelist:GetValue()+1]="加载出错请刷新lua列表"
+        else
+        ragename[ragelist:GetValue()+1]=string.split(ragename[ragelist:GetValue()+1],"]")[2]
+    end
+    ragelist:SetOptions( unpack(ragename) )
+end )
+unloadall:SetWidth(50)
+unloadall:SetPosY(95)
+unloadall:SetPosX(65)
+WindowTabBtt_1:SetWidth(50)
+WindowTabBtt_2:SetWidth(50)
+WindowTabBtt_3:SetWidth(50)
+legitlistf5:SetWidth(50)
+ragelistf5:SetWidth(50)
+legitload:SetWidth(50)
+legitload:SetPosY(95)
+legitload:SetPosX(130)
+rageload:SetWidth(50)
+rageload:SetPosY(95)
+rageload:SetPosX(130)
+
+callbacks.Register( "Draw", function ()
+    WindowTabBtt_1:SetDisabled(not QInEMaxCFGInvisible)
+    WindowTabBtt_2:SetDisabled(not QInEMaxLuaInvisible)
+    WindowTabBtt_3:SetDisabled(not QInEMaxMiscInvisible)
+    QInEMaxCFG:SetInvisible(QInEMaxCFGInvisible)
+    QInEMaxLua:SetInvisible(QInEMaxLuaInvisible)
+    QInEMaxMisc:SetInvisible(QInEMaxMiscInvisible)
+    Window:SetActive(aw_menu:IsActive())
+end)
